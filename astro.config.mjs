886a7f4c3106f760f91688d6833755610cd2b350
Eric Brattli <eric@brattli.net>
2024-07-12 17:49:40 +0200

    Updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
-import alpinejs from "@astrojs/alpinejs";
-
 export default defineConfig({
     site: 'https://camzymb.github.io',
-    integrations: [tailwind(), alpinejs()],
+    integrations: [tailwind()],
 });
